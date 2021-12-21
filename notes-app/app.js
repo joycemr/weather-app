7809afc88d6cf3fb51eae904141cc6f787265f3d
Michael Joyce <michael.joyce@illiniarts.com>
2021-12-21 09:59:25 -0600

    Latest

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,45 @@
-const fs = require('fs')
+const getNotes = require('./notes')
+const yargs = require('yargs')
 
-const file = 'notes.txt'
-const text = 'Line 7'
+yargs.version('1.1.0')
 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('Adding a new note')
+    }
+})
 
-try {
-    fs.appendFileSync(file, `\n${text}`)
-    console.log('The "data to append" was appended to file!');
-} catch (err) {
-    console.log(err);
-}
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Remove a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: () => {
+        console.log('Listing all notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('Reading a note')
+    }
+})
+
+// need this line for it all to work.
+// not a big fan of yargs at this point
+yargs.argv
+
+// this line also makes it work.
+// you have to access the .argv object
+// but it has to be after the above code
+// console.log(yargs.argv)
---
+++ notes-app/notes.js
@@ -0,0 +1,5 @@
+const getNotes = function () {
+    return 'TODO complete this function'
+}
+
+module.exports = getNotes
