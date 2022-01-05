e21d9338b19d1689b26341b5fab0fdaa6b601136
Michael Joyce <michael.joyce@illiniarts.com>
2022-01-05 16:44:51 -0600

    Refactor a little more readable

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,16 +4,14 @@ const forecast = require('./utils/forecast')
 
 geocode('Ofallon IL', (error, location) => {
     if (error) {
-        console.log(error)
-    } else {
-        forecast(location.lon, location.lat, (error, forecast) => {
-            if (error) {
-                console.log(error)
-            } else {
-                console.log(`${location.name} is located at ${location.lat} lat / ${location.lon} lon.`)
-                console.log(`Temperature: ${forecast.temperature}, Feels like: ${forecast.feelslike}, Conditions: ${forecast.description}`)
-            }
-        })
+        return console.log(error)
     }
+    forecast(location.lon, location.lat, (error, forecast) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log(`${location.name} is located at ${location.lat} lat / ${location.lon} lon.`)
+        console.log(`Temperature: ${forecast.temperature}, Feels like: ${forecast.feelslike}, Conditions: ${forecast.description}`)
+    })
 })
 
