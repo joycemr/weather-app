500f87f0c68a07764d8ef06b81496509431e0f1f
Michael Joyce <michael.joyce@illiniarts.com>
2022-01-05 16:42:18 -0600

    Refactor

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,11 +6,11 @@ geocode('Ofallon IL', (error, location) => {
     if (error) {
         console.log(error)
     } else {
-        console.log(`${location.name} is located at ${location.lat} lat / ${location.lon} lon.`)
         forecast(location.lon, location.lat, (error, forecast) => {
             if (error) {
                 console.log(error)
             } else {
+                console.log(`${location.name} is located at ${location.lat} lat / ${location.lon} lon.`)
                 console.log(`Temperature: ${forecast.temperature}, Feels like: ${forecast.feelslike}, Conditions: ${forecast.description}`)
             }
         })
