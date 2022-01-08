864aafd2d3d5a04e1306b7fc02c258a73ee28289
Michael Joyce <michael.joyce@illiniarts.com>
2022-01-08 15:09:41 -0600

    Switch to templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,15 +2,41 @@ const path = require('path')
 const express = require('express')
 
 
-const root = path.join(__dirname, '..', 'public')
-
 const app = express()
 
+// static directory setup
+const root = path.join(__dirname, '..', 'public')
 app.use('/', express.static(root))
-app.use('/about', express.static(path.join(root, 'about.html')))
-app.use('/help', express.static(path.join(root, 'help.html')))
 
-app.get('/Weather', (req, res) => {
+// handlebars setup
+const viewsPath = path.join(__dirname, '..', 'template')
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// paths
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Index Handlebars Page (Dynamic)',
+        name: 'Michael'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Dynamic Page',
+        name: 'Michael Joyce',
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Dynamic Page',
+        topic: 'This is the help topic',
+        name: 'Michael Joyce',
+    })
+})
+
+app.get('/weather', (req, res) => {
     res.send({
         location: 'O Fallon, IL',
         temperature: 19,
