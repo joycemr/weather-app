370ad4f021f69cb1a30c78aa8485f0413cc8e7e5
Michael Joyce <michael.joyce@illiniarts.com>
2022-01-09 09:42:17 -0600

    Refactor templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 
 const app = express()
@@ -9,39 +10,61 @@ const root = path.join(__dirname, '..', 'public')
 app.use('/', express.static(root))
 
 // handlebars setup
-const viewsPath = path.join(__dirname, '..', 'template')
+const viewsPath = path.join(__dirname, '..', 'template/views')
+const partialsPath = path.join(__dirname, '..', 'template/partials')
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+const creatorName = 'Michael Joyce'
 
 // paths
 app.get('/', (req, res) => {
     res.render('index', {
         title: 'Index Handlebars Page (Dynamic)',
-        name: 'Michael'
+        creatorName,
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Dynamic Page',
-        name: 'Michael Joyce',
+        creatorName,
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Dynamic Page',
+        creatorName,
         topic: 'This is the help topic',
-        name: 'Michael Joyce',
     })
 })
 
 app.get('/weather', (req, res) => {
     res.send({
+        title: 'Local Weather',
+        creatorName,
         location: 'O Fallon, IL',
         temperature: 19,
         conditions: ['cloudy',
                 'windy',],
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        creatorName,
+        errorMessage: 'Help page not found',
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        creatorName,
+        errorMessage: 'Page not found',
     })
 })
 
