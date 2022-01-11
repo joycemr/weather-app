d7deac5994e3dcaa71c557ea9242daa247ea8b3a
Michael Joyce <michael.joyce@illiniarts.com>
2022-01-11 09:55:59 -0600

    Add form and get address from it

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
-console.log('Client side javascript is loaded')
+// const weatherForm = document.querySelector('form')
+// const search = document.querySelector('input')
+const weatherForm = document.getElementById('addressForm')
+const search = document.getElementById('addressInput')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const address = search.value
+    const url = encodeURI(`http://127.0.0.1:3000/weather?address=${address}`)
+    console.log(`url: ${url}`)
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data)
+            }
+        })
+    })
+})
